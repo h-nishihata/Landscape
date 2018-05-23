435717d7e56e94c4f545f8beb3127617e6e504b6
h-nishihata <nishihata0301@gmail.com>
2018-05-23 15:04:21 +0900

    structures in progress

---
+++ Assets/Scripts/TransferObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransferObject : MonoBehaviour {
+
+    public GameObject startPosition;
+    public GameObject endPosition;
+    float startTime;
+    float translateTime = 60.0f;
+    Material mat;
+    Color tintColor;
+    Color targetColor;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = startPosition.transform.position;
+        startTime = Time.timeSinceLevelLoad;
+        mat = GetComponent<Renderer>().material;
+        targetColor = new Color(0.65f, 0.45f, 0.28f, 0.47f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var diff = Time.timeSinceLevelLoad - startTime;
+        if (diff > translateTime) {
+            transform.position = endPosition.transform.position;
+            enabled = false;
+        }
+        var rate = diff / translateTime;
+
+        tintColor = Color.Lerp(Color.black, targetColor, rate);
+        mat.SetColor("_TintColor", tintColor);
+        transform.position = Vector3.Lerp(startPosition.transform.position, endPosition.transform.position, rate);
+	}
+}
