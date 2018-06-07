a16aa0d9db2ce7a6686735480586c8e55cc4d461
h-nishihata <nishihata0301@gmail.com>
2018-06-07 15:01:25 -0400

    add fog particles

--- Assets/Scripts/WireFrame.cs
+++ Assets/Scripts/WireFrame.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 
 public class WireFrame : MonoBehaviour {
+    Camera wireFrameCam;
+
+    void Awake()
+    {
+        wireFrameCam = gameObject.GetComponent<Camera>();
+        // Only render objects in the first layer (Default layer)
+        wireFrameCam.cullingMask = 1 << 0;
+    }
+
     void OnPreRender() {
         GL.wireframe = true;
     }
+
     void OnPostRender() {
         GL.wireframe = false;
     }
