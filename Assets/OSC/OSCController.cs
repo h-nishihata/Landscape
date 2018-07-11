1191d58041f5ee0669037a292f2cbe216a310d75
h-nishihata <nishihata0301@gmail.com>
2018-07-11 11:49:57 -0400

    add OSC

---
+++ Assets/OSC/OSCController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityOSC;
+
+public class OSCController : MonoBehaviour {
+
+    private OSCHandler handler;
+	private OSCReciever reciever;
+	public int port = 6666;
+	
+	// Use this for initialization
+	void Start () {
+        handler = OSCHandler.Instance;
+        handler.Init();
+        handler.transform.SetParent(gameObject.transform);
+
+		reciever = new OSCReciever();
+		reciever.Open(port);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            handler.SendMessageToClient("Max", "/test", "Hello World");
+        }
+
+		if(reciever.hasWaitingMessages()){
+			OSCMessage msg = reciever.getNextMessage();
+			Debug.Log(string.Format("message received: {0} {1}", msg.Address, DataToString(msg.Data)));
+		}
+	}
+	
+    private string DataToString(List<object> data) {
+		string buffer = "";
+		
+		for(int i = 0; i < data.Count; i++)
+		{
+			buffer += data[i].ToString() + " ";
+		}
+		
+		buffer += "\n";
+		
+		return buffer;
+	}
+}
