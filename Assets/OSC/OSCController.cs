78f719f2a2e6ade1b944ab1c516c9c0efd4d01cf
h-nishihata <nishihata0301@gmail.com>
2018-07-28 13:26:07 -0400

    receive OSC and change colors

--- Assets/OSC/OSCController.cs
+++ Assets/OSC/OSCController.cs
@@ -6,42 +6,44 @@ using UnityOSC;
 public class OSCController : MonoBehaviour {
 
     private OSCHandler handler;
-	private OSCReciever reciever;
+    private OSCReceiver receiver;
     public string address = "172.16.202.207";
 	public int sendPort = 7400;
     public int receivePort = 6666;
+    public ControllCamColor camColor;
 	
-	// Use this for initialization
+
 	void Start () {
         handler = OSCHandler.Instance;
         handler.Init("Max", address, sendPort);
         handler.transform.SetParent(gameObject.transform);
-
-		reciever = new OSCReciever();
-		reciever.Open(receivePort);
-	}
+		receiver = new OSCReceiver();
+		receiver.Open(receivePort);
+	}	
 	
-	// Update is called once per frame
 	void Update () {
-		if(reciever.hasWaitingMessages()) {
-			OSCMessage msg = reciever.getNextMessage();
-			Debug.Log(string.Format("message received: {0} {1}", msg.Address, DataToString(msg.Data)));
-		}
-	}
+		if(receiver.hasWaitingMessages()) {            
+			OSCMessage msg = receiver.getNextMessage();
+            if (msg.Address == "/transition") {
+                ReceiveMessages(msg.Data);
+                //}else{
+                //Debug.LogError("cannot receive the message.");
+            }
+        }
+    }
 	
     public void SendMessages(string address, float value) {
         handler.SendMessageToClient("Max", address, value);
     }
 
-    private string DataToString(List<object> data) {
-		string buffer = "";
-		
+    private void ReceiveMessages(List<object> data) {
 		for(int i = 0; i < data.Count; i++) {
-			buffer += data[i].ToString() + " ";
+            int num;
+            bool b = int.TryParse(data[i].ToString(), out num);
+
+            if(b){
+                camColor.ControlTransition(num);
+            }
 		}
-		
-		buffer += "\n";
-		
-		return buffer;
 	}
 }
---
+++ Assets/Scripts/ControllCamColor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllCamColor : MonoBehaviour {
+    
+    private float startTime;
+    private float transitionTime = 1 / 10.0f;
+    private float restorationTime = 1 / 2.0f;
+    private bool isChangingColor;
+    private bool isClimax;
+    //private Camera baseCam;
+    private VideoBloom bloom;
+    private Color tintColor;
+    private Color normalColor;
+
+
+	void Start () {
+        //baseCam = this.GetComponent<Camera>();
+        bloom = this.GetComponent<VideoBloom>();
+        normalColor = new Color(0.75f, 0.57f, 0.4f, 0.4f);
+	}
+
+    public void ControlTransition(int type){
+        switch (type)
+        {
+            case 0:
+                isChangingColor = false;
+                break;
+            case 1:
+                isChangingColor = true;
+                startTime = Time.timeSinceLevelLoad;
+                isClimax = true;
+                break;
+            case 2:
+                isChangingColor = true;
+                startTime = Time.timeSinceLevelLoad;
+                isClimax = false;
+                break;            
+        }
+    }
+
+	void Update () {
+        if(isChangingColor) {
+            var diff = Time.timeSinceLevelLoad - startTime;
+            var rate = isClimax ? diff * transitionTime : diff * restorationTime;
+
+            if (rate > 1.0f) return;
+
+            if (isClimax) {                
+                tintColor = Color.Lerp(normalColor, Color.white, rate);
+                //baseCam.backgroundColor = Color.Lerp(Color.black, Color.white, rate);
+            }else{
+                tintColor = Color.Lerp(Color.white, normalColor, rate);
+            }
+            bloom.Tint = tintColor;
+        }
+	}
+}
